f8b726d81f47b67ef5f49e312ee3e6343975b5d8
Samson Nelson <snelson@inmoment.com>
2016-06-01 13:39:35 -0600

    Navigate to SLC - Click on first image - console.log general information

--- test/my-first-test.js
+++ test/my-first-test.js
@@ -4,13 +4,16 @@ var client = webdriverio.remote(options);
 
 client
     .init()
-    .url('https://duckduckgo.com/')
-    .setValue('#search_form_input_homepage', 'WebdriverIO')
-    .click('#search_button_homepage')
+    .url('http://www.loopnet.com/')
+    // .setValue('#search_form_input_homepage', 'WebdriverIO')
+    .setValue('/html/body/section/main/section[1]/section[1]/div/div/div/form/div/div/div[2]/div[3]/div/input' , 'Salt Lake City, Utah')
+    .click('/html/body/section/main/section[1]/section[1]/div/div/div/form/div/div/div[2]/div[4]/button[1]')
     .getTitle().then(function(title) {
         console.log('Title is: ' + title);
-
-        // outputs:
-        // "Title is: WebdriverIO (Software) at DuckDuckGo"
+    })
+    .refresh()
+    .click('//*[@id="placardSec"]/div[2]/div[1]/article[1]/div[1]/section[1]/div/div/div/div/div[2]/div/figure')
+    .getText('/html/body/section/main/section/div/div/div/div/div/section[1]/div/div/div').then(function(text) {
+      console.log(text);
     })
     .end();
